a53bcb69cf131eb16ed8812207110f9c93f17844
vish9151 <84173784+vishal9151@users.noreply.github.com>
2023-11-13 11:24:25 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import {Home,Login,SignUp} from './components';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
-  const { authenticated, login } = useTodo();
+  const { authenticated} = useTodo();
 
   return (
     <Router>
       <Routes>
         <Route
           path="/"
-          element={authenticated ? <Home /> : <Navigate to="/signup" />}
+          element={authenticated ? <Home /> : <SignUp/>}
         />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
